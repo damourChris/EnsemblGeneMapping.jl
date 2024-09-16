a8e28a08c862f5745cb6e2a00722fc12aafa700f
Christopher <129872252+damourChris@users.noreply.github.com>
2024-09-16 14:01:52 +0000

    feat: add main package logic

--- src/EnsemblGeneMapping.jl
+++ src/EnsemblGeneMapping.jl
@@ -1,5 +1,41 @@
 module EnsemblGeneMapping
 
-# Write your package code here.
+using Comonicon
+using RCall
+using ExpressionData
+
+@main function map_to_ensembl(eset::String, attribute::String;
+                              output_file::Union{Nothing,String}=nothing,
+                              config_file::Union{Nothing,String}=nothing)
+    # Parse the arguments into a config struct
+    config::Config = load_config(config_file)
+
+    output_file = isnothing(output_file) ? eset : output_file
+
+    eset = load_eset(eset)
+
+    (; gene_col, mart_id, mart_dataset) = config
+
+    @rput eset gene_col mart_id mart_dataset attribute
+
+    R"""
+    mart <- biomaRt::useMart(mart_id, dataset = mart_dataset)
+
+    annotated_eset <-
+        remove_empty_genes(eset, gene_col = gene_col) %>%
+        extract_first_gene_symbol(gene_col = gene_col) %>%
+        aggregate_expression(gene_col = gene_col) %>%
+        map_to_ensembl(
+            gene_col = gene_col,
+            attribute = attribute,
+            mart = mart
+            ) %>%
+        aggregate_expression(gene_col = "ensembl_id")
+    """
+
+    eset = @rget annotated_eset
+
+    return save_eset(eset, output_file)
+end
 
 end
